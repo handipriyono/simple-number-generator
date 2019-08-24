bbee597d6887966e2c1783b516022378aca7b7b7
handi <handimacbook@gmail.com>
2019-08-24 07:29:22 +0700

    init number generator

---
+++ index.js
@@ -0,0 +1,8 @@
+function randomNoGenerator(min, max) {
+  if (typeof max !== "number" && typeof min !== "number") {
+    min = 0;
+    max = 1;
+  }
+  return Math.random() * (max - min) + min;
+}
+module.exports = randomNoGenerator;
