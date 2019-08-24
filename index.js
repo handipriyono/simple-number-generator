406b0a2ff27f29629dceb17cf1133f4d39f7153b
handi <handimacbook@gmail.com>
2019-08-24 16:11:53 +0700

    update simple random number generator funct

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-function randomNoGenerator(min, max) {
-  if (typeof max !== "number" && typeof min !== "number") {
+function randomNoGenerator(params) {
+  if (!params) {
+    params = {
+      min: 0,
+      max: 1
+    };
+  }
+
+  let { min, max, integer } = params;
+
+  if (!min && !max) {
     min = 0;
     max = 1;
   }
-  return Math.random() * (max - min) + min;
+
+  if (!min && min !== 0) {
+    if (typeof max !== "number") {
+      max = 1;
+    }
+    min = max - 1;
+  }
+
+  if (!max && max !== 0) {
+    if (typeof min !== "number") {
+      min = 0;
+    }
+    max = min + 1;
+  }
+
+  if (min > max) {
+    return "max. must be greater than min.";
+  }
+
+  if (integer) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  } else {
+    return Math.random() * (max - min) + min;
+  }
 }
 module.exports = randomNoGenerator;
